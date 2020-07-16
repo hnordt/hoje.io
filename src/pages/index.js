a635c5ed205e1090a9125680cfc0d8146b43c985
Heliton Nordt <hnordt@hnordt.com>
2020-07-16 20:48:02 -0300

    chore: Add USDBRL

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,30 @@
 import React from "react"
+import { GoogleSpreadsheet } from "google-spreadsheet"
 import Layout from "components/Layout"
 
-export default function Home() {
+export async function getStaticProps() {
+  const doc = new GoogleSpreadsheet(process.send.GOOGLE_SHEETS_SHEET_ID)
+
+  doc.useApiKey(process.env.GOOGLE_SHEETS_API_KEY)
+  await doc.loadInfo()
+
+  const sheet = doc.sheetsByIndex[0]
+
+  await sheet.loadCells("B1:B5")
+
+  const usdbrl = sheet.getCellByA1("B1").value
+
+  return {
+    props: {
+      usdbrl,
+    },
+  }
+}
+
+export default function Home(props) {
   return (
     <Layout title="Home">
-      <h1>It works!</h1>
+      <h1>{props.usdbrl}</h1>
     </Layout>
   )
 }
